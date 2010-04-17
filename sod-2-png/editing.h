4351e4b8cc6dd1166ac59fad32b94818d45d3c21
xj <xj@kanjisod.kicks-ass.net>
2010-04-17 18:54:08 +0900

    deleted old files

--- sod-2-png/editing.h
+++
@@ -1,7 +0,0 @@
-#include <libxml/tree.h>
-#import "arg.h"
-
-/*  xml editing header file */
-
-void print_element_names(xmlNode* a_node);
-void process_xml_options(xmlNode* node, argo *selected_opts);
